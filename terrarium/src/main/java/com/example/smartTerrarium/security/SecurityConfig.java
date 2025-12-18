749f18b70b401268e7f9390a29354007b54d48d0
Zuzia <zuzanna.leszczynska1@gmail.com>
2025-12-18 11:52:49 +0100

    login service

--- terrarium/src/main/java/com/example/smartTerrarium/security/SecurityConfig.java
+++ terrarium/src/main/java/com/example/smartTerrarium/security/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/users/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
