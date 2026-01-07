5d77629277b7361c50496e38a772b62013fca431
Zuzia <zuzanna.leszczynska1@gmail.com>
2026-01-07 19:22:26 +0100

    fix for login

--- terrarium/src/main/java/com/example/smartTerrarium/dto/UserSendData.java
+++ terrarium/src/main/java/com/example/smartTerrarium/dto/UserSendData.java
@@ -1,7 +1,11 @@
 package com.example.smartTerrarium.dto;
 
-import lombok.Builder;
+import lombok.*;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Builder
 public class UserSendData {
     private String email;
