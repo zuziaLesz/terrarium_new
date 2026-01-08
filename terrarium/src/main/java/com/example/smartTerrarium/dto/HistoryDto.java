b503570999488739c64a779fc1501b6988096da9
Zuzia <zuzanna.leszczynska1@gmail.com>
2026-01-08 03:49:22 +0100

    history dto added

--- terrarium/src/main/java/com/example/smartTerrarium/dto/HistoryDto.java
+++ terrarium/src/main/java/com/example/smartTerrarium/dto/HistoryDto.java
@@ -1,9 +1,6 @@
 package com.example.smartTerrarium.dto;
 
-import com.sun.jna.platform.win32.Sspi;
-
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Map;
 
 public class HistoryDto {
