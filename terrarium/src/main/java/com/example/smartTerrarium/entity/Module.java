8287582303f0f639b873e47ae861f63b6c9b6b84
Zuzia <zuzanna.leszczynska1@gmail.com>
2026-01-18 10:59:15 +0100

    delete water_min i max, status to state

--- terrarium/src/main/java/com/example/smartTerrarium/entity/Module.java
+++ terrarium/src/main/java/com/example/smartTerrarium/entity/Module.java
@@ -30,9 +30,5 @@ public class Module {
     private String groupId;
     @Column(name = "last_edit_date")
     private LocalDateTime lastEditDate;
-    @Column(name="water_min")
-    private String waterMin;
-    @Column(name="water_max")
-    private String waterMax;
     private Double intensity;
 }
